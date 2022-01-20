cdd14a9ce9017ab9ec69480c48564a4459d07298
Lucas Miranda <lucasalternativo.lg@gmail.com>
2022-01-20 17:54:29 -0300

    Changed lexicon's compiler logic

--- server/lexicon-compiler/lexicon-compiler.js
+++ server/lexicon-compiler/lexicon-compiler.js
@@ -2,62 +2,35 @@ const readLine = require("readline");
 const fs = require("fs");
 
 exports.compile = async () => {
-  // holds every entry in the lexicon
-  let entries = [];
+  const entries = {};
 
-  // open lexicon's master file
-  let lineReaderMaster = readLine.createInterface({
-    input: fs.createReadStream("./portilex/WORDmaster.txt"),
-  });
+  files = fs.readdirSync("./portilex");
 
-  // iterate through the master file lines (which are words)
-  for await (const word of lineReaderMaster) {
-    // saves a word record (has the lexicon info about it)
-    let wordRec = {};
+  for (file of files) {
+    const lineReader = readLine.createInterface({
+      input: fs.createReadStream(`./portilex/${file}`),
+    });
 
-    // split in the comma
-    let [form, posTags] = word.split(",");
+    const posTag = file.slice(1, -4);
 
-    // store word's form and place to put a feature array with lexical info
-    // of different pos tags
-    wordRec["word"] = form;
-    wordRec["lexicalInfo"] = [];
+    for await (const entry of lineReader) {
+      const [form, lemma, feats] = entry.split("\t");
 
-    // splits word's pos tags
-    posTags = posTags.split(" ");
-
-    // iterate through word's pos tags
-    for (let posTag of posTags) {
-      let lineReaderPosTag;
-      // open pos tag's file
-      lineReaderPosTag = readLine.createInterface({
-        input: fs.createReadStream(`./portilex/_${posTag}.tsv`),
-      });
-
-      // iterates through file's lines
-      let foundIt = false;
-      for await (const entry of lineReaderPosTag) {
-        let [posTagWordForm, lemma, feats] = entry.split("\t");
-
-        // stores word's pos tag, lemma and feature
-        if (form === posTagWordForm) {
-          wordRec["lexicalInfo"].push({
-            posTag: posTag,
-            lemma: lemma,
-            features: feats,
-          });
-          foundIt = true;
-        } else if (foundIt) {
-          break;
-        }
+      if (!(form in entries)) {
+        entries[form] = [];
       }
-    }
 
-    entries.push(wordRec);
-    console.log(entries.length);
+      entries[form].push({ posTag, lemma, feats });
+    }
   }
 
-  console.log("Writing compiled lexicon...");
-  fs.writeFileSync("./portilex.json", JSON.stringify(entries));
-  console.log("Lexicon compiled successfully!");
+  let finalEntries = [];
+  for (word in entries) {
+    finalEntries.push({
+      word,
+      lexicalInfo: entries[word],
+    });
+  }
+
+  return finalEntries;
 };
