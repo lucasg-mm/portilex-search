ebc5eb08ac63729cedc3b55074b33b284a29dc34
Lucas Miranda <lucasalternativo.lg@gmail.com>
2022-01-24 17:23:17 -0300

    Added initial styling

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    <h1>Portilex</h1>
-    <input v-model="searchTerm" type="text" />
-    <button @click="makeSearch">Pesquisar</button>
+    <h1>Portilexicon</h1>
+    <span class="p-input-icon-left">
+      <i class="pi pi-search" />
+      <InputText
+        type="text"
+        v-model="searchTerm"
+        placeholder="Search for a word"
+      />
+    </span>
+    <Button @click="makeSearch" label="Search" />
     <div v-for="result in searchResults" :key="result._id">
       <div v-for="info in result.lexicalInfo" :key="info._id">
         <h2>{{ result.word }}</h2>
@@ -17,7 +24,12 @@
 </template>
 
 <script>
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+
 export default {
+  components: { InputText, Button },
+
   data() {
     return {
       // stores the search term
@@ -56,4 +68,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+body {
+  font-family: "Roboto", sans-serif;
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
 
-createApp(App).mount('#app')
+// imports PrimeVue Styles
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+// creates
+const app = createApp(App);
+
+// uses
+app.use(PrimeVue);
+
+// mounts app
+app.mount("#app");
