c626b1f909ecbb903973a3ad72f91e7bf3c788d8
Lucas Miranda <lucasalternativo.lg@gmail.com>
2022-02-23 19:32:01 -0300

    Added UD link to initial page's description

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,8 +12,11 @@
                   >The PortiLexicon-UD lexicon contains over 1.2 million word
                   forms in Portuguese, presented with their respective
                   morphological and morphosyntactic classifications, using the
-                  set of labels from the Universal Dependencies framework.
-                  PortiLexicon-UD is based on
+                  set of labels from the
+                  <a href="https://universaldependencies.org/"
+                    >Universal Dependencies</a
+                  >
+                  framework. PortiLexicon-UD is based on
                   <a
                     href="http://www.nilc.icmc.usp.br/nilc/projects/unitex-pb/web/index.html"
                     >Unitex-PB</a
